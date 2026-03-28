fd3760114eeea309f3583c3531ed1990524bbd1e
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2026-03-28 19:31:13 +0100

    feat: implement IReportService and ReportService for rental report generation

--- ConsoleApp1/service/report/IReportService.cs
+++ ConsoleApp1/service/report/IReportService.cs
@@ -1,6 +1,8 @@
-﻿namespace ConsoleApp1.service.report;
+﻿using ConsoleApp1.Domain.Rental;
+
+namespace ConsoleApp1.service.report;
 
 public interface IReportService
 {
-    
+    RentalReportSummaryDTO GenerateSummary();
 }
