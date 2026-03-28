3b28e8475eeef0879e724013175bafc1e6feaacc
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2026-03-28 18:36:55 +0100

    feat: implement IUserService and UserService for user management

--- ConsoleApp1/service/user/UserService.cs
+++ ConsoleApp1/service/user/UserService.cs
@@ -1,5 +1,35 @@
-﻿namespace ConsoleApp1.service.user;
+﻿using ConsoleApp1.Domain.User;
 
-public class UserService
+namespace ConsoleApp1.service.user;
+
+public class UserService : IUserService
 {
+    private readonly List<UserDTO> _users = new();
+    private int _nextId = 1;
+
+    public UserDTO AddStudent(string firstName, string lastName, string studentNumber)
+    {
+        int id = _nextId++;
+        var student = new StudentDTO(id, firstName, lastName, studentNumber);
+        _users.Add(student);
+        return student;
+    }
+
+    public UserDTO AddEmployee(string firstName, string lastName, string studentNumber)
+    {
+        int id = _nextId++;
+        var employee = new EmployeeDTO(id, firstName, lastName, studentNumber);
+        _users.Add(employee);
+        return employee;
+    }
+
+    public UserDTO GetByid(int id)
+    {
+        return _users.FirstOrDefault(u => u.Id == id) ?? throw new Exception($"User with id {id} not found.");
+    }
+
+    public List<UserDTO> GetAll()
+    {
+        return _users.ToList();
+    }
 }
